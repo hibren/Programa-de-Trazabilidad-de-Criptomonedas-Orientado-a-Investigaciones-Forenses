83766fc1b9acf776eb625da8b51d6003da182c23
acostamarianela <acostagm6@gmail.com>
2025-09-18 20:51:38 -0300

    importar desde api connected

--- frontend/src/services/direccionesService.js
+++ frontend/src/services/direccionesService.js
@@ -7,14 +7,16 @@ export const getDirecciones = async () => {
 }
 
 export const fetchDireccionFromAPI = async (address) => {
-  const res = await fetch(`${API_URL}/fetch`, {
+  const res = await fetch(`${API_URL}fetch`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ direccion: address }),
   })
+
+  const data = await res.json() // 👈 leer solo una vez
   if (!res.ok) {
-    const error = await res.json()
-    throw new Error(error.detail || "Error importando dirección")
+    throw new Error(data.detail || "Error importando dirección")
   }
-  return res.json()
+  return data
 }
+
