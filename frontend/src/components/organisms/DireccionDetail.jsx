29705429a7f2e7bb50a3988f84054728a96e24f0
acostamarianela <acostagm6@gmail.com>
2025-09-19 22:30:38 -0300

    sin error

--- frontend/src/components/organisms/DireccionDetail.jsx
+++ frontend/src/components/organisms/DireccionDetail.jsx
@@ -72,6 +72,7 @@ export default function DireccionDetail({ direccion }) {
               { accessorKey: "estado", header: "Estado" },
               { accessorKey: "confirmations", header: "Confirmaciones" },
             ]}
+            filterColumn="hash"
           />
         </div>
       </div>
